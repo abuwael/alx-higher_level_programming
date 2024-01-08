edc1183da0809a1616e650af8993d6b707081e3d
abuwael <mahmoudwaeldev@gmail.com>
2024-01-08 11:46:36 -0800

    Lookup

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""
+Author: Abdullah Bedwey
+Date: 10/7/2023
+"""
+
+
+def lookup(obj):
+    """
+    Returns a list of available attributes and methods of an object.
+    """
+    return dir(obj)
