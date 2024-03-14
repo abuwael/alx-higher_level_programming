218a4ef5f41a3748011026af46277ebd8ea9b48a
abuwael <mahmoudwaeldev@gmail.com>
2024-03-13 17:12:04 -0700

    0x01. Python - if/else, loops, functions

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+ldigit = abs(number) % 10
+if number < 0:
+    ldigit = -ldigit
+print(f"Last digit of {number:d} is {ldigit:d} and is ", end="")
+if ldigit > 5:
+    print("greater than 5")
+elif ldigit == 0:
+    print("0")
+else:
+    print("less than 6 and not 0")
